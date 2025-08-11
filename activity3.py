5c5fdf95e3a286e4daa195aef4ea5de204633b51
unknown <robertoguevarrabsit1a@gmail.com>
2025-08-11 19:24:12 +0800

    Code Challenge

--- activity3.py
+++ activity3.py
@@ -1,3 +1,3 @@
 name = input("What is your name .?")
 
-print("\t\t\t\t* \n\n \t\t\t*\t\t* \n\n \t\t*\t\t\t\t*\n\n \t*\t\t\t Hello \t\t\t* \n\n \t\t\t\t", name,"\t\t\t\t \n\n \t*\t\t\t\t\t\t* \n\n \t\t*\t\t\t\t* \n\n \t\t\t*\t\t* \n\n \t\t\t\t*")
+print("\t\t\t\t* \n\n \t\t\t*\t\t* \n\n \t\t*\t\t\t\t*\n\n \t*\t\t\t LOVE U\t\t\t* \n\n \t\t\t\t", name,"\t\t\t\t \n\n \t*\t\t\t\t\t\t* \n\n \t\t*\t\t\t\t* \n\n \t\t\t*\t\t* \n\n \t\t\t\t*")
