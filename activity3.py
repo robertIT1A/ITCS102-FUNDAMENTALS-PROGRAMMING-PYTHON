a65543101fa74727d73a3f37e72b80ab4df135a5
robertIT1A <robertoguevarrajr25@gmail.com>
2025-08-12 18:27:57 +0800

    Update and rename activity3.py to code_challenge1.py

--- activity3.py
+++
@@ -1,3 +0,0 @@
-name = input("What is your name .?")
-
-print("\t\t\t\t* \n\n \t\t\t*\t\t* \n\n \t\t*\t\t\t\t*\n\n \t*\t\t\t LOVE U\t\t\t* \n\n \t\t\t\t", name,"\t\t\t\t \n\n \t*\t\t\t\t\t\t* \n\n \t\t*\t\t\t\t* \n\n \t\t\t*\t\t* \n\n \t\t\t\t*")
