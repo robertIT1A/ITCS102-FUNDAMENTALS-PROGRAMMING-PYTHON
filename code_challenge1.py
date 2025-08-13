1fc4c550dcd80d83942a8d0ccb592d8cfa4276d4
robertIT1A <robertoguevarrajr25@gmail.com>
2025-08-13 10:24:49 +0800

    code_challenge1.py

--- code_challenge1.py
+++ code_challenge1.py
@@ -1,1 +1,3 @@
-print("\t\t\t\t\t♦ \n\n \t\t\t\t♦\t\t♦ \n\n \t\t\t♦\t\t\t\t♦ \n\n \t\t♦\t\t\tLOVE U\t\t\t♦ \n\n \t♦\t\t\t\tRoberto\t\t\t\t♦ \n\n \t\t♦\t\t\t \t\t\t♦ \n\n \t\t\t♦\t\t\t\t♦ \n\n \t\t\t\t♦\t\t♦ \n\n \t\t\t\t\t♦")
+name = input("Type your name:")
+
+print("\t\t\t\t\t♦ \n\n \t\t\t\t♦\t\t♦ \n\n \t\t\t♦\t\t\t\t♦ \n\n \t\t♦\t\t\tLOVE U\t\t\t♦ \n\n \t♦\t\t\t\t",name,"\t\t\t\t♦ \n\n \t\t♦\t\t\t \t\t\t♦ \n\n \t\t\t♦\t\t\t\t♦ \n\n \t\t\t\t♦\t\t♦ \n\n \t\t\t\t\t♦")
