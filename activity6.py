08298823cc29204c7441dbb270d0f20525a45804
robertIT1A <robertoguevarrajr25@gmail.com>
2025-08-15 15:13:48 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,13 @@
+x = eval(input("Enter the value of x : "))
+y = eval(input("Enter the value of y : "))
+s = x + y
+d = x - y
+p = x * y
+q = x / y
+print("\nThe sum of", x, "and", y, "is" ,s)
+print("The difference of", x, "and", y, "is", d) 
+print("The product of", x, "and",y, "is",p) 
+print("The quotient of",x, "and",y, "is",q)
+print(x, "exponent by", y, "is",x**y)
+print ("The remainder of", x, "and",y, "is", x % y) 
+print ("The floor division of",x, "and", y, "is", x//y)
