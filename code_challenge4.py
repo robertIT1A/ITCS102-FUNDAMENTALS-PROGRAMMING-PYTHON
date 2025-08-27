9f0ba5780a44aea4773a79efb26b4f8f3157a84c
robertIT1A <robertoguevarrajr25@gmail.com>
2025-08-27 15:19:35 +0800

    Add files via upload

---
+++ code_challenge4.py
@@ -0,0 +1,8 @@
+#Determine if a whole number is even or odd
+number = int(input("Enter a number: "))
+
+if number % 2 == 0: #If your number divided into 2 it should have 0 remainder
+    print("Even Number")
+
+else:
+    print("Odd Numbers")
