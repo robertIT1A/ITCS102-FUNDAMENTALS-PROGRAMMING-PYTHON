1fa8f03d5ab948148b4e78186bca927c144b98f4
robertIT1A <robertoguevarrajr25@gmail.com>
2025-09-13 17:22:32 +0800

    Delete activity13.py

--- activity13.py
+++
@@ -1,9 +0,0 @@
-#ask the user to input a number using loop
-#contatenate all the input numbers
-
-con = []
-
-for i in range (1, 16):
-    x = int(input("Enter Whole Number: "))
-    con.append (x) #append para pumunta sa []
-    print(con)
