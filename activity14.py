eee541f072884491d515414fb04cff9a004d284f
robertIT1A <robertoguevarrajr25@gmail.com>
2025-09-13 18:55:50 +0800

    Reverse loop

--- activity14.py
+++ activity14.py
@@ -4,11 +4,12 @@
 con = []
 
 
-for tanong in range (1, 5):
+for tanong in range (1, 16):
     x = int(input("Enter a number: "))
     con.append (x)
 
 
 
 for reverse in reversed (con):
+
     print(reverse, end="") #end para mag leave ng space
