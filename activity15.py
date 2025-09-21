a83c990338d07d9e1edf0dd059fc45bdfe8d8a6c
robertIT1A <robertoguevarrajr25@gmail.com>
2025-09-21 19:07:46 +0800

    f-string
    f-string in Python is a way to put a variables in just a one single bracket
    it will avoid some syntax error

---
+++ activity15.py
@@ -0,0 +1,10 @@
+# f-string in Python is a way to put a variables in just a one single bracket
+# it will avoid some syntax error
+
+name = "Roberto Guevarra"
+age = 19
+course = "Bachelor of Science in Information Technology"
+school = "Dalubhasaan ng Lungsod ng Lucena"
+
+print(f"My name is {name} and I am {age} years old. I study at {school} with a course of {course}.")
+
