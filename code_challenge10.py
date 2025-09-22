2f93cf003a2195909a282be3b18c77dd591cc4e3
robertIT1A <robertoguevarrajr25@gmail.com>
2025-09-22 18:19:59 +0800

    Mid Triangle
    make a triangle using kalso. Kalso help to reverse a loop and make space for a loop

---
+++ code_challenge10.py
@@ -0,0 +1,14 @@
+# make a triangle
+# using kalso
+
+
+for outer in range(1,11,1):
+    for kalso in range(10,outer,-1): # it will print reverse triangle 
+        print(" ",end=" ") # but because of " " is will provide space/blank
+    for inner in range(0,outer,1):
+        print("*",end=" ") #therefore "*" is only visible, but it will print reverse because of kalso
+    for inner in range(1,outer,1):
+        print("*",end=" ")
+    for kalso in range(10,outer,-1):
+        print(" ",end=" ")
+    print()
