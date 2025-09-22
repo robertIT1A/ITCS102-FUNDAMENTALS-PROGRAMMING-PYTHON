e3cfbe5a029dcdc81990375063fe3580f969d6bf
robertIT1A <robertoguevarrajr25@gmail.com>
2025-09-22 18:14:51 +0800

    Right Triangle
    create a right triangle using (*) in loop

---
+++ activity19.py
@@ -0,0 +1,6 @@
+# create a right triangle using (*)
+
+for outer in range(1,11,1): 
+    for inner in range(1, outer,1): # print triangle using (*)
+        print("*", end=" ") 
+    print() # to make new line after each row
