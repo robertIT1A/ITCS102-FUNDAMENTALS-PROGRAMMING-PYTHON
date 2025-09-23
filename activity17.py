d9c0717a4716e38461c5f2f00c8aa55fb9f3b048
robertIT1A <robertoguevarrajr25@gmail.com>
2025-09-23 14:58:31 +0800

    1 to 10 box

--- activity17.py
+++ activity17.py
@@ -4,6 +4,7 @@
 for sun in range(1,11,1): #This will serve as a outer loop 
     for earth in range(1,11,1): #This will serve as a inner loop 
         # Each time the outer loop runs, the inner loop runs from 1 to 10 
-        print(earth,)  # print earth, followed by end=" " to prevent new line
+        print(earth, end=" ")  # print earth, followed by end=" " to prevent new line
     print()# print new line every inner loop
     
+
