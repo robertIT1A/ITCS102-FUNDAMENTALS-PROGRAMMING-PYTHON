e36b59f62d3c0bfb5d8de5588c29126d81d2f3af
robertIT1A <robertoguevarrajr25@gmail.com>
2025-10-22 11:16:22 +0800

    Refactor code to handle odd number input

--- code_challenge14.py
+++ code_challenge14.py
@@ -1,26 +1,30 @@
-# ask the user to enter an even number
-# if the user input a odd number print "Invalid Number"
+# ask the user to enter an odd number
+# if the user input a even number print "Invalid Number"
 # stop the asking if the user input 0
-# print the sum of all the input even numbers
-# list all the input even numbers
+# print the sum of all the input odd numbers
+# list all the input odd numbers
 
 
 name = input("Enter your name: ")
-print("-"*40)
-num = 1
-add = 0
-con = "\r"
 
-while num != 0:
-    num = int(input("Enter a number: "))
-    if num % 2 == 0:
-        print("Even Number")
-        add += num 
-        con += str(num) + ","
+odd = 0
+con = "" # Empty list
+proceed = True
 
+while proceed == True:
+    num = eval(input("Enter a number: "))
+    if num % 2 == 1:
+        print("Odd Detected")
+        odd += num
+        con += str(num) + "," # To concatenate all the odd number that the user enter
+        continue
+    elif num == 0: # Stop while the user input 0
+        print("Loop Terminated")
+        break
     else:
-        print("Invalid number") 
+        print("Even detected")
+        print("Skipping")
+        continue
 
-print("Loop Terminated")
-print(f"Hi {name}, The sum of all the EVEN numbers is {add}")
-print(f"All the EVEN numbers you input is {con}")
+print(f"Hi {name}, The sum of all the EVEN numbers is {odd}")
+print(f"All odd number: {con}")
