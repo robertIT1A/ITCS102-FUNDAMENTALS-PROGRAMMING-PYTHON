dd429e30e58c8b255440cc6c39c15071fd21f20b
robertIT1A <robertoguevarrajr25@gmail.com>
2025-11-14 20:44:07 +0800

    dictionary: Love Language

--- activity26.py
+++ activity26.py
@@ -2,6 +2,7 @@
 
 Language = {'Filipino': 'Mahal kita', 'English':'I Love You', 'French': "Je t'aime", 'Spanish': 'Te amo ', 'German': 'Ich liebe dich', 'Italian': 'Ti amo', 'Japanese': 'Aishiteru', 'Mandarin Chinese': 'Wǒ ài nǐ', 'Korean': 'Saranghae', 'Russian': 'Ya lyublyu tebya', 'Arabic': 'Ana uhibbuka', 'Portuguese':' Eu te amo'}
 
+prin("LOVE LANGUAGE")
 print("\n143 in every language")
 for i,j in Language.items():
     print(f"\tLanguage for 143 -- {i}")
@@ -10,4 +11,5 @@ print({Language[love]})
 
 # notes
 # pero pag gusto mo na madami ilalagay sa value gagamit ka ng [] after ng key and :
+
 # {Keys:[Values madami]}
