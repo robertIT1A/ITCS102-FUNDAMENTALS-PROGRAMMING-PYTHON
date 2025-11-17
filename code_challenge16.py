f194e9b9f0417f3399ac3a6722c70433008da4dd
robertIT1A <robertoguevarrajr25@gmail.com>
2025-11-17 21:15:16 +0800

    dictionary with key and values

--- code_challenge16.py
+++ code_challenge16.py
@@ -1,4 +1,5 @@
 import os
+import time
 # os.sytstem('cls)
 print("Student Information System")
 print("---------------------------")
@@ -7,11 +8,17 @@ print("---------------------------")
 student_list = {}
 
 def review_fav_list():
+    print("This will ends in....")
     for i,s in student_list.items():
         print(f"Nickname: {i}, Favorite Person: {s}")
 
+def countdown():
+  for i in range(5):
+    print(i)
+    time.sleep(1)
+
 while True:
-  print("Select the option below:\n A Add information\n B - Search a Record\n C -  Delete a Record\n D - Review a Record")
+  print("Select the option below:\n A - Add information\n B - Search a Record\n C -  Delete a Record\n D - Review a Record")
 
   option = input("Enter your option: ").lower()
   
@@ -21,20 +28,32 @@ while True:
     last_name = input("Enter Last name: ").capitalize()
     course = input("Student Course: ")
 
-    student_list = {Search_cod : [first_name, last_name, course]}
-    print(student_list)
+    # student_list = {Search_cod : [first_name, last_name, course]}
+    student_list[Search_cod] = [first_name, last_name, course]
     print("DATA SAVED")
+    os.system('cls')
 
   elif option == "b":
     code = input("Enter Student Id: ")
     for i in student_list.keys():
       for code in student_list.keys():
-        print("Record Found")
+        print(code)
+    print("Record Found")
+    os.system('cls')
     continue
   elif option == "c":
     code = input("Enter Student Id: ")
-    remove = student_list.pop(code)
+    if code == Search_cod:
+        remove = student_list.pop(code)
+        print(f"Student{Search_cod} is remove to the list")
+        countdown()
+        os.system('cls')
+    else:
+      print("The Student Id is not found")
   elif option == "d":
     review_fav_list()
+    countdown()
+    os.system('cls')
   else:
     print("Invalid")
+    os.system('cls')
