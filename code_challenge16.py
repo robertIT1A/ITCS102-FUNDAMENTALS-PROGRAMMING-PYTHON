b402dea537dcb13f32b9558ae7ff0c6f0df08016
robertIT1A <robertoguevarrajr25@gmail.com>
2025-11-18 20:51:14 +0800

    dictionary with key and values

--- code_challenge16.py
+++ code_challenge16.py
@@ -8,9 +8,22 @@ print("---------------------------")
 student_list = {}
 
 def review_fav_list():
-    print("This will ends in....")
     for i,s in student_list.items():
-        print(f"Nickname: {i}, Favorite Person: {s}")
+        print(f"Student Id: {i}, Student Name: {s}")
+    print("This will ends in....")
+
+def delete():
+  # code = input("Enter Student ID to delete: ")
+  # if code in student_list:
+  #   removed_student = student_list.pop(code)  # Safely remove by key
+  #   print(f"Student ID {code} ({removed_student[0]} {removed_student[1]}{removed_student[2]}) has been removed from the list.")
+  #   countdown()
+  #   os.system('cls')
+
+  remove = student_list.remove(code)
+  print(f"Student Id number {Search_cod} is remove to the list")
+  countdown()
+  os.system('cls')
 
 def countdown():
   for i in range(5):
@@ -18,7 +31,7 @@ def countdown():
     time.sleep(1)
 
 while True:
-  print("Select the option below:\n A - Add information\n B - Search a Record\n C -  Delete a Record\n D - Review a Record")
+  print("Select the option below:\n A - Add information\n B - Search a Record\n C -  Delete a Record\n D - Review a Record\n E - Modify the List")
 
   option = input("Enter your option: ").lower()
   
@@ -35,25 +48,24 @@ while True:
 
   elif option == "b":
     code = input("Enter Student Id: ")
-    for i in student_list.keys():
-      for code in student_list.keys():
-        print(code)
+    print(f"Result shows is {student_list[code]}")
     print("Record Found")
+    countdown()
     os.system('cls')
     continue
   elif option == "c":
     code = input("Enter Student Id: ")
-    if code == Search_cod:
-        remove = student_list.pop(code)
-        print(f"Student{Search_cod} is remove to the list")
-        countdown()
-        os.system('cls')
+    if code in student_list: # dito nagsearch na ako pano mawala yung pinaka unang keys kasi yung last lagi yung pede lang madelete
+      del student_list[code]
+        # delete()
     else:
       print("The Student Id is not found")
   elif option == "d":
     review_fav_list()
     countdown()
     os.system('cls')
+  elif option == "e":
+     pass
   else:
     print("Invalid")
     os.system('cls')
