1eec9a311225f59e233f24efe04c994d51d9e0a6
robertIT1A <robertoguevarrajr25@gmail.com>
2025-11-30 20:14:09 +0800

    data types

--- activity5.py
+++ activity5.py
@@ -1,7 +1,8 @@
-something = eval(input("Input number:"))
+something = eval(input("Input number: "))
 
 print("The data type of something is", type(something))
 
 answer = 23 + something
+
 
 print("The answer is ", answer)
